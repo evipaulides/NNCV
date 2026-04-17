a749bbf1ec3dd78e518e05576e707c47953ce440
evipaulides <e.paulides@student.tue.nl>
2026-04-17 12:50:49 +0200

    les epochs

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -6,7 +6,7 @@ pip install torchmetrics
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 128 \
-    --epochs 200 \
+    --epochs 150 \
     --num-workers 10 \
     --seed 42 \
     --experiment-id "unet-augmentation" \
