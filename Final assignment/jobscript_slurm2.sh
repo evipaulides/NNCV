3df9fb68a0d0e9e558491de72832f016e5300346
evipaulides <e.paulides@student.tue.nl>
2026-04-26 19:36:52 +0200

    Cleaning up repo 2

--- Final assignment/jobscript_slurm2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=18
-#SBATCH --gpus=1
-#SBATCH --partition=gpu_h100
-#SBATCH --time=36:00:00
-
-srun apptainer exec --nv --env-file .env container.sif /bin/bash main2.sh
